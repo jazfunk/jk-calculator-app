165582fdb6b1b7804588c13d47d6107d3a53f4c8
Jeff King <jazfunk@gmail.com>
2020-05-03 13:32:39 -0400

    Button click displays button value for all buttons.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const numberButtons = document.querySelectorAll('.number-btn');
-
 numberButtons.forEach(button => {  
   button.addEventListener('click', (e) => {
     debugger;
@@ -9,11 +8,28 @@ numberButtons.forEach(button => {
 });
 
 const operatorButtons = document.querySelectorAll('.operator-btn');
-
 operatorButtons.forEach(button => {
   button.addEventListener('click', (e) => {
     debugger;
     document.getElementById('input-display').value = e.target.innerText;
     console.log(e.target.innerText);
-  })
-})
+  });
+});
+
+const memoryButtons = document.querySelectorAll('.memory-btn');
+memoryButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    debugger;
+    document.getElementById('input-display').value = e.target.innerText;
+    console.log(e.target.innerText);
+  });
+});
+
+const clearButtons = document.querySelectorAll('.clear-btn');
+clearButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    debugger;
+    document.getElementById('input-display').value = e.target.innerText;
+    console.log(e.target.innerText);
+  });
+});
