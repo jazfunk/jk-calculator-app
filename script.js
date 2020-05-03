e87b167a9ca07b3b0d371186d196ad6fb5367440
Jeff King <jazfunk@gmail.com>
2020-05-03 18:07:05 -0400

    All math functions working.  Refactoring and debugging continues.

--- script.js
+++ script.js
@@ -1,49 +1,109 @@
-
-let number1 = 0;
-let number2 = 0
+let number1 = "0";
+let number2 = "0";
 let operator = "";
+let numberMemory = "0";
+let isDecimal = false;
 
 window.onload = () => {
   addButtonEventListeners();
+};
 
+const numberButtons = document.querySelectorAll(".number-btn");
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const buttonPressed = e.target.innerText;
+    let displayValue = "0";
+
+    if (buttonPressed === ".") {
+      if (isDecimal) {
+        return;
+      }
+      isDecimal = true;
+    }
+
+    if (operator === "") {
+      if (number1 === "0") {
+        number1 = buttonPressed;
+      } else {
+        number1 += buttonPressed;
+      }
+      displayValue = number1;
+    } else {
+      if (number2 === "0") {
+        number2 = buttonPressed;
+      } else {
+        number2 += buttonPressed;
+      }
+      displayValue = number2;
+    }
+
+    document.getElementById("input-display").value = displayValue;
+  });
+});
+
+let clearValues = () => {
+  number1 = "0";
+  number2 = "0";
+  isDecimal = false;
+  operator = "";
 }
 
+let calculateInput = () => {
+  try {
+    switch (operator) {
+      case "X":
+        return parseFloat(number1) * parseFloat(number2);
+      case "/":
+        return parseFloat(number1) / parseFloat(number2);
+      case "+":
+        return parseFloat(number1) + parseFloat(number2);
+      case "-":
+        return parseFloat(number1) - parseFloat(number2);
+    }
+  } catch (error) {
+    return -1;
+  }
+};
 
-function addButtonEventListeners() {
-
-  const numberButtons = document.querySelectorAll(".number-btn");
-  numberButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      // debugger;
-      document.getElementById("input-display").value = e.target.innerText;
-      console.log(e.target.innerText);
-    });
+const operatorButtons = document.querySelectorAll(".operator-btn");
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (number2 === "0") {
+      operator = e.target.innerText;
+      isDecimal = false;
+    } else {
+      document.getElementById("input-display").value = calculateInput();
+      clearValues();
+      number1 = document.getElementById("input-display").value;
+    }
   });
+});
 
-  const operatorButtons = document.querySelectorAll(".operator-btn");
-  operatorButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      debugger;
-      document.getElementById("show-operator").innerText = e.target.innerText;
-      console.log(e.target.innerText);
-    });
-  });
+const memoryButtons = document.querySelectorAll(".memory-btn");
+memoryButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // debugger;
+    if (e.target.innerText === "M+") {
+      numberMemory = document.getElementById("input-display").value;
+      console.log(numberMemory);
+    }
 
-  const memoryButtons = document.querySelectorAll(".memory-btn");
-  memoryButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      debugger;
-      document.getElementById("show-operator").innerText = e.target.innerText;
-      console.log(e.target.innerText);
-    });
-  });
+    if (e.target.innerText === "MR") {
+      document.getElementById("input-display").value = numberMemory;
+    }
 
-  const clearButtons = document.querySelectorAll(".clear-btn");
-  clearButtons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      debugger;
-      document.getElementById("show-operator").innerText = e.target.innerText;
-      console.log(e.target.innerText);
-    });
+    if (e.target.innerText === "MC") {
+      numberMemory = "0";
+    }
   });
-}
+});
+
+const clearButtons = document.querySelectorAll(".clear-btn");
+clearButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // debugger;
+    if (e.target.innerText === "C") {
+      clearValues();
+    }
+  });
+});
