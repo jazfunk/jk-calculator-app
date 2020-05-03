dee51c4e534dec00a275d6755b19d6d95bbf80ec
Jeff King <jazfunk@gmail.com>
2020-05-03 14:47:54 -0400

    Number and Operator buttons aligned, no math fx yet.

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-const numberButtons = document.querySelectorAll('.number-btn');
-numberButtons.forEach(button => {  
-  button.addEventListener('click', (e) => {
-    debugger;
-    document.getElementById('input-display').value = e.target.innerText;
-    console.log(e.target.innerText);
-  });
-});
 
-const operatorButtons = document.querySelectorAll('.operator-btn');
-operatorButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    debugger;
-    document.getElementById('input-display').value = e.target.innerText;
-    console.log(e.target.innerText);
-  });
-});
+let number1 = 0;
+let number2 = 0
+let operator = "";
 
-const memoryButtons = document.querySelectorAll('.memory-btn');
-memoryButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    debugger;
-    document.getElementById('input-display').value = e.target.innerText;
-    console.log(e.target.innerText);
-  });
-});
+window.onload = () => {
+  addButtonEventListeners();
 
-const clearButtons = document.querySelectorAll('.clear-btn');
-clearButtons.forEach(button => {
-  button.addEventListener('click', (e) => {
-    debugger;
-    document.getElementById('input-display').value = e.target.innerText;
-    console.log(e.target.innerText);
+}
+
+
+function addButtonEventListeners() {
+
+  const numberButtons = document.querySelectorAll(".number-btn");
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      // debugger;
+      document.getElementById("input-display").value = e.target.innerText;
+      console.log(e.target.innerText);
+    });
   });
-});
+
+  const operatorButtons = document.querySelectorAll(".operator-btn");
+  operatorButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      debugger;
+      document.getElementById("show-operator").innerText = e.target.innerText;
+      console.log(e.target.innerText);
+    });
+  });
+
+  const memoryButtons = document.querySelectorAll(".memory-btn");
+  memoryButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      debugger;
+      document.getElementById("show-operator").innerText = e.target.innerText;
+      console.log(e.target.innerText);
+    });
+  });
+
+  const clearButtons = document.querySelectorAll(".clear-btn");
+  clearButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      debugger;
+      document.getElementById("show-operator").innerText = e.target.innerText;
+      console.log(e.target.innerText);
+    });
+  });
+}
